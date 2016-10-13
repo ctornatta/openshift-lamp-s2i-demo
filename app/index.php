8ef426120789de24e36089275374a6c0b2bf8d89
Chris Tornatta <ctornatta@virtusa.com>
2016-10-13 09:59:17 -0400

    switched to mysqli

--- app/index.php
+++ app/index.php
@@ -5,20 +5,29 @@ $db_name = getenv('DB_NAME');
 $db_pass = getenv('DB_PASS');
 $db_user = getenv('DB_USER');
 
+# debug
+#echo "database host: $db_host <br>";
+#echo "database name: $db_name <br>";
+#echo "<br><br>";
+
 #connection to the database
 $dbhandle = new mysqli($db_host, $db_user, $db_pass, $db_name);
 if ($dbhandle->connect_errno) {
     echo "Failed to connect to MySQL: (" . $dbhandle->connect_errno . ") " . $dbhandle->connect_error;
+    exit;
 }
-echo $dbhandle->host_info . "\n";
+
+# debug info
+#echo $dbhandle->host_info . "\n";
 
 #Execute Query
 $result = $dbhandle->query("SELECT id, name, class, government FROM starship");
 
+echo "<H1>Star Trek Starships</H1>";
 #Display data
 while ($row = $result->fetch_assoc()) {
-   echo "ID:".$row{'id'}." Name:".$row{'class'}."Government: ".$row{'government'}."<br>";
+   echo "<b>Row:</b> ".$row{'id'}."<br><b>Name:</b> ".$row{'name'}."<br><b>Class:</b> ".$row{'class'}."<br><b>Government:</b> ".$row{'government'}."<br><br>";
 }
-//close the connection
-mysql_close($dbhandle);
+#close the connection
+mysqli_close ( $dbhandle );
 ?>
